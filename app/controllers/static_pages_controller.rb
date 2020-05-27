21b7560b8ad286cdd45ff0cccde94cc163615ae7
Murilo Roque <muriloengqui@gmail.com>
2020-05-27 20:55:03 -0300

    create flikr in static controller and show in home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,15 @@
 class StaticPagesController < ApplicationController
-  def home
+  require 'flickraw'
+  FlickRaw.api_key = ENV['FLICKRAW_API_KEY']
+  FlickRaw.shared_secret = ENV['FLICKRAW_SHARED_SECRET']
 
+  def home
+    list   = flickr.photos.getRecent
+    id     = list[0].id
+    secret = list[0].secret
+    @info = flickr.photos.getInfo photo_id: id, secret: secret
+    sizes = flickr.photos.getSizes photo_id: id
+    @original = sizes.find { |s| s.label == 'Original' }
+    @photos = flickr.photos.search(user_id: '188655404@N06')
   end
 end
