ba07d3655051c0c4c6fb57fd34ddfa9fec2d585c
Murilo Roque <muriloengqui@gmail.com>
2020-05-28 07:13:56 -0300

    finish photo widget

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,15 @@
 class StaticPagesController < ApplicationController
   require 'flickraw'
-  FlickRaw.api_key = ENV['FLICKRAW_API_KEY']
-  FlickRaw.shared_secret = ENV['FLICKRAW_SHARED_SECRET']
 
   def home
-    list   = flickr.photos.getRecent
-    id     = list[0].id
-    secret = list[0].secret
-    @info = flickr.photos.getInfo photo_id: id, secret: secret
-    sizes = flickr.photos.getSizes photo_id: id
-    @original = sizes.find { |s| s.label == 'Original' }
-    @photos = flickr.photos.search(user_id: '188655404@N06')
+    unless search_params.nil?
+      @photos = flickr.photos.search(user_id: search_params[:id])
+    end
+  end
+
+  private
+
+  def search_params
+    params.require(:search).permit(:id) if params[:search].present?
   end
 end
---
+++ config/initializers/flickraw.rb
@@ -0,0 +1,2 @@
+FlickRaw.api_key = ENV['FLICKRAW_API_KEY']
+  FlickRaw.shared_secret = ENV['FLICKRAW_SHARED_SECRET']
